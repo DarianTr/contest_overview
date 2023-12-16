c51f1391ea4ef77e30ed5415ac00e348deb4c872
DarianTr <dlt7@unamail.net>
2023-12-16 23:36:29 +0100

    made contest package

--- main.go
+++ main.go
@@ -6,22 +6,19 @@ import (
 	"net/http"
 )
 
-var client *http.Client
-var domjAPIToken string
-
 type Data struct {
 	Contest []contest.Contest
 	Judges  []string
 }
 
 func main() {
-	// SetJudges()
-	// SetDomjAPIToken()
-	// UpdateContests()
+	contest.SetJudges()
+	contest.SetDomjAPIToken()
+	contest.UpdateContests()
 
-	// http.HandleFunc("/", h1)
-	// http.HandleFunc("/search", h2)
-	// http.HandleFunc("/options", h3)
+	http.HandleFunc("/", h1)
+	http.HandleFunc("/search", h2)
+	http.HandleFunc("/options", h3)
 	http.HandleFunc("/calendar", displayCalendar)
 	http.HandleFunc("/c", displayCalendar)
 
